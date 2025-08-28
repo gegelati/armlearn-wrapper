b3ea71dc38c49fe4a5e0beed2be6b0a5e5aa1d69
Dardaillon Mickael <mickael.dardaillon@insa-rennes.fr>
2025-08-28 15:09:42 +0200

    Fix missing override after update.

--- src/ArmLearnWrapper.h
+++ src/ArmLearnWrapper.h
@@ -239,7 +239,7 @@ public:
 
 
     /// @brief Inherited via LearningEnvironment
-    void doAction(double actionID);
+    virtual void doAction(double actionID) override;
 
     /// Do a multi continuous action.
     void doActionContinuous(std::vector<float> actions);
